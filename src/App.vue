a4e28a1717274a3f672e3e8bdf0d0dbf97b9e331
VoxCrafterLP <voxcrafterlp@gmail.com>
2022-02-03 01:00:00 +0100

    Port v3 version to v2

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,56 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <navbar></navbar>
   </div>
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Navbar from './components/Navbar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+      Navbar
   }
 }
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+body {
+  color: black;
+  /* background-color: #1a1a22; */
+  margin: 0;
+  padding: 0;
 }
+
+/* Global variables */
+:root {
+  font-size: 16px;
+  font-family: 'Roboto', sans-serif;
+  --text-primary: #ececec;
+  --text-secondary: #b6b6b6;
+  --bg-primary: #23232e;
+  --bg-secondary: #141418;
+  --bg-accent: #32324d;
+  --transition-speed: 600ms;
+  --accent-color: #b33aa8;
+}
+
+/* Customize scrollbar */
+body::-webkit-scrollbar {
+  width: 0.25rem;
+}
+
+body::-webkit-scrollbar-track {
+  background: #1e1e24;
+}
+
+body::-webkit-scrollbar-thumb {
+  background: #6649b8;
+}
+
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,143 @@
+<template>
+    <div>
+        <nav class="navbar">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <font-awesome-icon icon="home" class="nav-link-icon"></font-awesome-icon>
+                        <span class="nav-link-text">Overview</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <font-awesome-icon icon="chess" class="nav-link-icon"></font-awesome-icon>
+                        <span class="nav-link-text">Chess game</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="https://github.com/VoxCrafterLP/symmetry-day/" class="nav-link">
+                        <font-awesome-icon :icon="['fab', 'github']" class="nav-link-icon"></font-awesome-icon>
+                        <span class="nav-link-text">Source code</span>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "Navbar",
+    props: [],
+    methods: {
+        
+    }
+}
+
+</script>
+
+<style>
+.navbar {
+    position: fixed;
+    background-color: var(--bg-primary);
+    transition: width 200ms ease;
+}
+
+.navbar-nav {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 100%;
+}
+
+.nav-item {
+    width: 100%;
+}
+
+.nav-item:last-child {
+    margin-top: auto;
+}
+
+.nav-link {
+    display: flex;
+    align-items: center;
+    height: 5rem;
+    color: var(--text-secondary);
+    text-decoration: none;
+    transition: var(--transition-speed);
+}
+
+.nav-link:hover {
+    background: var(--bg-secondary);
+    color: var(--text-primary);
+}
+
+.nav-link-icon {
+    color: var(--text-secondary);
+    transition: var(--transition-speed);
+}
+
+.nav-link:hover svg {
+    color: var(--accent-color);
+    transform: scale(1.2);
+}
+
+.nav-link-text {
+    display: none;
+    margin-left: 1rem;
+}
+
+.nav-link:hover span {
+    color: var(--text-primary);
+}
+
+.nav-link svg {
+    min-width: 2rem;
+    margin: 0 1.5rem;
+    font-size: 1.8rem;
+}
+
+
+/* Mobile */
+@media only screen and (max-width: 600px) {
+    .navbar {
+        bottom: 0;
+        width: 100vw;
+        height: 5rem;
+    }
+
+    .navbar-nav {
+        flex-direction: row;
+    }
+
+    .nav-link {
+        justify-content: center;
+    }
+
+    main {
+        margin: 0;
+    }
+}
+
+/* Desktop */
+@media only screen and (min-width: 600px) {
+    .navbar {
+        top: 0;
+        width: 5rem;
+        height: 100vh;
+    }
+
+    .navbar:hover {
+        width: 16rem;
+    }
+
+    .navbar:hover .nav-link-text {
+        display: inline;
+    }
+}
+
+</style>
